d3d01428629f9b87d80631d289ead968544e0c63
Aaron Sinnott <aaronsnig@gmail.com>
2020-12-01 11:21:57 -0300

    test(button): add additional Button test

--- src/components/__test__/Button.snapshot.test.tsx
+++ src/components/__test__/Button.snapshot.test.tsx
@@ -1,9 +1,30 @@
 
 import React from 'react';
-import Button from '../elements/Button';
 import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
+import Button from '../elements/Button';
 
-it(`renders correctly`, () => {
+it(`renders a primary colored button correctly`, () => {
   const tree = renderer.create(<Button text="Some Text" color="primary" />).toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+it(`renders a rounded primary colored button correctly`, () => {
+  const tree = renderer.create(<Button text="Some Text" color="primary" variant="rounded" />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it(`renders a rounded primary colored button with a shadow correctly`, () => {
+  const tree = renderer.create(<Button text="Some Text" color="primary" variant="rounded" modifier="shadow" />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it(`renders a button if 'as' parameter is not provided`, () => {
+  const { getByTestId } = render(<Button text="Some Text" color="primary" variant="rounded" modifier="shadow" />);
+  expect(getByTestId('button')).toBeTruthy();
+});
+
+it(`renders a link if 'as' parameter is 'a'`, () => {
+  const { getByTestId } = render(<Button text="Some Text" color="primary" variant="rounded" modifier="shadow" as="a" />);
+  expect(getByTestId('link')).toBeTruthy();
 });
