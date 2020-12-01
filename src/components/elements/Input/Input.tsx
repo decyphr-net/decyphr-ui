8ae55d00a92222c6d2491d9e2261fed743da374a
Aaron Sinnott <aaronsnig@gmail.com>
2020-12-01 18:29:02 -0300

    feat(input component): basic implementation of Input component

---
+++ src/components/elements/Input/Input.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+type Props = {
+  type?: string;
+  placeholder?: string;
+  as?: string;
+  children?: JSX.Element;
+}
+
+
+const Input = ({type, placeholder, as, children, ...rest}: Props) => {
+  if (as === "select") {
+    return (
+      <div className="select">
+        <select>
+          {children}
+        </select>
+      </div>
+    )
+  } else {
+    return (
+      <input className="input" {...rest} />
+    )
+  }
+};
+
+export default Input;
