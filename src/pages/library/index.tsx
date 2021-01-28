7408aa9fc0766920e18bd5345d0e462d1169b92c
Aaron Sinnott <aaronsnig@gmail.com>
2021-01-28 19:29:24 -0300

    feat(library): create initial structure for library page

--- src/pages/library/index.tsx
+++ src/pages/library/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import { useIntl } from 'react-intl';
 import DashboardLayout from '../../components/layouts/dashboard';
+import Autocomplete from '../../components/elements/Autocomplete';
 
 const Library: React.FC = () => {
   const { formatMessage } = useIntl();
@@ -11,7 +12,42 @@ const Library: React.FC = () => {
 
   return (
     <DashboardLayout>
-      <p>Hello world</p>
+      <div className="column is-8">
+        <h2 className="has-text-left is-size-2">Library</h2>
+        <h3 className="has-text-left is-size-3 mt-4">Add a new item to your library</h3>
+        <Autocomplete
+          name="library"
+          label=""
+          placeholder="Select an item to add to your library"
+          data={[
+            'Apple',
+            'Pineapple',
+            'Banana',
+            'Orange',
+            'Peach',
+            'Blueberry',
+            'Pear',
+          ]}
+        />
+
+        <h3 className="has-text-left is-size-3 mt-6">Continue a book from your library</h3>
+        <table className="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
+        <thead>
+          <tr>
+            <th>Cover</th>
+            <th>Title</th>
+            <th>Options</th>
+          </tr>
+        </thead>
+        <tr>
+        <td>Cover</td>
+            <td>Title</td>
+            <td>Options</td>
+        </tr>
+      </table>
+      </div>
+
+
     </DashboardLayout>
   )
 }
